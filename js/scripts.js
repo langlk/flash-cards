dcb9d63f4908b74ebd65ced975bb7fd15d6c5d4a
Kelsey Langlois and Megan Olson <megan@>
2017-08-08 15:09:45 -0700

    Add flashcard grid with changing colors for clicks

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $("li").click(function() {
     $(this).children(".term").toggle();
     $(this).children(".definition").toggle();
+    if ($(this).hasClass("green-background")) {
+      $(this).removeClass("green-background");
+    } else {
+      $(this).addClass("green-background");
+    }
   });
 });
